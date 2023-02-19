0305207a71a4c82b686c408b0804a12379eb0ea5
Wanderley <100Palavr@gmail.com>
2023-02-19 02:00:11 -0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express'
-import prisma from './project/util/prismaClient'
 import userRouter from './project/user/routes/routes'
-import body from 'body-parser'
+import { checkAuthentication } from './project/authMiddleware/auth';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 app.use(express.raw());
+app.use(checkAuthentication);
 app.use('/user', userRouter);
 
 app.listen(port, () => {
